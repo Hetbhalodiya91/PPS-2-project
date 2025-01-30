d2ceb59706b2bcfd98ef6a749146a20d75ffa927
Hetbhalodiya91 <bhalodiyahet9126@gmail.com>
2025-01-30 08:53:04 +0530

    4

--- hotelM.cpp
+++ hotelM.cpp
@@ -2,7 +2,17 @@
 
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
-    cout<<n;
+    int n [10];
+
+    for(int i=0; i<10; i++){
+        cin>>n[i];
+    }
+
+for (int i = 0; i < 10; i++)
+{
+    /* code */
+    cout<<n[i];
+}
+
+    
 }
